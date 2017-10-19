2050e4b14879540d0851c7867328216da4b2e906
FannyLAJ <fanny.lajeun@gmail.com>
2017-10-19 16:39:19 +0200

    Update LoginAction.inc.php

--- CODE/actions/LoginAction.inc.php
+++ CODE/actions/LoginAction.inc.php
@@ -4,19 +4,30 @@ require_once("actions/Action.inc.php");
 
 class LoginAction extends Action {
 
-	/**
-	 * Traite les données envoyées par le visiteur via le formulaire de connexion
-	 * (variables $_POST['nickname'] et $_POST['password']).
-	 * Le mot de passe est vérifié en utilisant les méthodes de la classe Database.
-	 * Si le mot de passe n'est pas correct, on affiche le message "Pseudo ou mot de passe incorrect."
-	 * Si la vérification est réussie, le pseudo est affecté à la variable de session.
-	 *
-	 * @see Action::run()
-	 */
-	public function run() {
-  	/* TODO START */
-  	/* TODO END */
-	}
+    /**
+     * Traite les données envoyées par le visiteur via le formulaire de connexion
+     * (variables $_POST['nickname'] et $_POST['password']).
+     * Le mot de passe est vérifié en utilisant les méthodes de la classe Database.
+     * Si le mot de passe n'est pas correct, on affiche le message "Pseudo ou mot de passe incorrect."
+     * Si la vérification est réussie, le pseudo est affecté à la variable de session.
+     *
+     * @see Action::run()
+     */
+
+    public function run() {
+        if (isset($_POST['nickname']) && isset($_POST['password'])) {
+            $nickname = $_POST['nickname'];
+            $password = $_POST['password'];
+            $res = $this->database->checkPassword($nickname, $password);
+            if ($res != true) {
+                $this->setMessageView("Pseudo ou mot de passe incorrect");
+            }
+            else {
+                $this->setSessionLogin($nickname);
+                $this->setView(getViewByName("Default"));
+            }
+        }
+    }
 
 }
 
