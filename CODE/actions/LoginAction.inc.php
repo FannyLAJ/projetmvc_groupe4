a48b3b4824c124ce260de9cf494966218266dbd7
FannyLAJ <fanny.lajeun@gmail.com>
2017-10-19 22:34:02 +0200

    Update LoginAction.inc.php

--- CODE/actions/LoginAction.inc.php
+++ CODE/actions/LoginAction.inc.php
@@ -15,20 +15,27 @@ class LoginAction extends Action {
      */
 
     public function run() {
+
+        //Si les données du formulaire existent, on les traite
         if (isset($_POST['nickname']) && isset($_POST['password'])) {
             $nickname = $_POST['nickname'];
             $password = $_POST['password'];
-            $res = $this->database->checkPassword($nickname, $password);
+
+            //On instancie un objet Database et on fait appel à sa méthode checkPassword.
+            //Si la méthode checkPassword retourne false, on affiche un message d'erreur.
+            //Sinon, on stocke le login en session et on notifie l'utilisateur qu'il est connecté.
+            $db = new Database();
+            $res = $db->checkPassword($nickname, $password);
             if ($res != true) {
-                $this->setMessageView("Pseudo ou mot de passe incorrect");
+                $this->setMessageView("Pseudo ou mot de passe incorrect", "alert-error");
             }
             else {
                 $this->setSessionLogin($nickname);
-                $this->setView(getViewByName("Default"));
+                $this->setView(getViewByName("Message"));
+                $this->getView()->setMessage("Connexion réussie.", "alert-success");
             }
         }
     }
-
 }
 
 ?>
